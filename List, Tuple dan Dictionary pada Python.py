1f008b0c5f465d2bba21eb3deccb056990a38d8f
hidayaturrahman21 <spydayat5@gmail.com>
2024-10-22 00:57:41 +0800

    Create List, Tuple dan Dictionary pada Python.py

---
+++ List, Tuple dan Dictionary pada Python.py
@@ -0,0 +1,41 @@
+no 1
+list warna
+# List warna
+warna = ["Merah", "Hijau", "Biru", "Kuning"]
+
+# Menampilkan list warna
+print(warna)
+
+no 2
+dictionary untuk menyimpan data buku
+
+  # Membuat tuple data siswa
+siswa = (
+    ("Nama", "Usia", "Kelas"),
+    ("Budi", 15, "10A"),
+    ("Siti", 14, "9B"),
+    ("Andi", 16, "11C")
+)
+
+# Menampilkan data siswa
+for data in siswa:
+    print(data)
+
+no 3
+dictionary untuk menyimpan data buku
+# Membuat dictionary untuk menyimpan data buku
+buku = {
+    "Judul": "keganasan anggota gang mall",
+    "Penulis": "braco,isi,hi",
+    "Tahun": 2023,
+    "Penerbit": "Penerbit pareman"	
+}
+
+# Menampilkan data buku
+print("Data Buku:")
+print("Judul:", buku["Judul"])
+print("Penulis:", buku["Penulis"])
+print("Tahun:", buku["Tahun"])
+print("Penerbit:", buku["Penerbit"])
+
+
